5a32f0fa6fe092f0a2d2227d1ed2ac555ca32837
GabrielMalakias <gabriel07malakias@gmail.com>
2014-12-22 18:54:14 -0200

    refactor

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 
 RSpec.describe TweetsController, :type => :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Tweet. As you add validations to Tweet, be sure to
-  # adjust the attributes here as well.
+
   let(:valid_attributes) {
     skip("Add a hash of attributes valid for your model")
   }
@@ -14,9 +12,6 @@ RSpec.describe TweetsController, :type => :controller do
     skip("Add a hash of attributes invalid for your model")
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # TweetsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
   describe "GET index" do
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "Parse of hash attributes" do 
+		tweet = Tweet.new
+		time = Time.now
+		hash = {:retweet_count => 2, :favorite_count => 2, 
+    	:text => "Go RoR", :created_at => time, :tweet_id => '465465545646'}
+    	tweet.parse(hash)
+		expect(tweet.retweet_count).to eq(2)
+		expect(tweet.favorite_count).to eq(2)
+		expect(tweet.text).to eq("Go RoR")
+		expect(tweet.created_at).to eq(time)
+		expect(tweet.tweet_id).to eq('465465545646')
+	end
 end
